59b59455b17ad79ff8d61499394ed56bd1de5a07
harshthakur9004 <thakur.harsh4953@gmail.com>
2025-10-31 22:03:30 +0530

    today changes

---
+++ javascript/1_variables.js
@@ -0,0 +1,16 @@
+// value and variable in javascript
+
+// in javascript , values and variable are fundamental concept that from the basic of programming
+//  value , a  value is a piece of information that a program can work with . it can be a number , text, true/ false ,or  more complex data
+
+//  a variable , is a container
+
+//  let's test 
+ var myName="Harsh Thakur";
+ console.log(myName);
+ var my_fristName="john";
+ console.log(my_fristName);
+ var _myLastName$="doe";
+ console.log(_myLastName$);
+var $cityName="new york";
+console.log($cityName);
---
+++ javascript/2_datatype.js
@@ -0,0 +1,32 @@
+//  it has tow type of datatype 
+//  primitive datatype , objective datatype
+
+//  primitive datatype
+// string , number ,boolean, undefined, null, big int , symbol 
+
+//  an object , an array, a date
+
+// number example
+var myAge=22;
+console.log(myAge);
+
+// string example
+var myName="Harsh Thakur";
+console.log(myName);
+
+// boolean example 
+var isRain="false";
+console.log(isRain);
+
+//undefined example
+var myPhone;
+console.log(myPhone);
+
+//✅ Interview-Friendly One-Line Answer:
+
+// undefined means a variable has been declared but not assigned a value.
+// null is an assignment value that represents an intentional empty value.
+
+// big int example
+var myMoney=1234565785654322;
+console.log(myMoney);
